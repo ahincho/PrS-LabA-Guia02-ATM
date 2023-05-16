ab07a7cc9786f96b16ba8f9f37eb23df5652a4f0
dneirac <dneirac@unsa.edu.pe>
2023-05-16 11:40:31 -0500

    Agregando metodos de deposito, retiro y ver salario

--- new-atm/new_atm.py
+++ new-atm/new_atm.py
@@ -4,11 +4,34 @@
 @author: ahincho
 @author: dneira 
 """
-
 import os
+from user import User
 
 class Cajero:
-    def __init__(self):
-        pass
+    def __init__(self, user):
+        self.set_user(user)
+
+    def set_user(self, user):
+        self.user = user
+
+    def get_user(self):
+        return self.user
     
-app = Cajero()
+    def withdraw(self, amount_withdraw):
+        if amount_withdraw > 3000:
+            raise ValueError("Amount must be less than 3000")
+        if self.user.get_salary() <  amount_withdraw: 
+            raise ValueError("Amount must be less than salary")
+        self.user.set_salary(self.user.get_salary()-amount_withdraw)
+        return self.user.get_salary()
+
+    def deposit(self, amount_deposit):
+        if amount_deposit > 3000:
+            raise ValueError("Amount must be greater than 3000")
+        self.user.set_salary(self.user.get_salary()+amount_deposit)
+        return self.user.get_salary()
+
+    def view_salary(self):
+        return self.user.get_salary()
+    
+#app = Cajero()
