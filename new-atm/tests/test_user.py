b9fe5976f3da5c28b799ec7daad738c0a45e8283
ahincho <ahincho@unsa.edu.pe>
2023-05-16 10:15:18 -0500

    Declarando las Pruebas Unitarias para Usuario

--- new-atm/tests/test_user.py
+++ new-atm/tests/test_user.py
@@ -1,6 +1,25 @@
 
+from src.user import User
 import unittest
 
+# Observaciones
+# - Debe almacenar su monto y password
+# Pruebas Unitarias:
+# - Un caso donde se establecio bien el salario
+# - Un caso donde se establecio bien el password
+# - Un caso donde se intente agregar un salario negativo
+# - Un caso donde se el password sea menor a 4 caracteres
+
 class TestUser(unittest.TestCase):
-    def should_be_true(self):
-        self.assertTrue(True)
+    def test_set_salary_1000(self):
+        user = User(1000, "1234")
+        self.assertEquals(user.get_salary(), 1000)
+    def test_set_password_1234(self):
+        user = User(1000, "1234")
+        self.assertEquals(user.get_password(), "1234")
+    def test_set_negative_salary(self):
+        with self.assertRaises(TypeError):
+            user = User(-1000, "1234")
+    def test_set_invalid_password(self):
+        with self.assertRaises(TypeError):
+            usert = User(1000, "123")
