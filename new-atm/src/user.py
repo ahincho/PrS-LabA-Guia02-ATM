7dedae9ebc631911793e3162987e5c201864c4ca
dneirac <dneirac@unsa.edu.pe>
2023-05-16 10:25:51 -0500

    Agregando setter al usuario

--- new-atm/src/user.py
+++ new-atm/src/user.py
@@ -1,9 +1,18 @@
 
 class User():
-    def __init__(self, salary, password):
+    def set_salary(self, salary):
+        if salary < 0:
+            raise ValueError("Salary must be postive")
         self.salary = salary
+    def set_password(self, password):
+        if len(password) < 4:
+            raise ValueError("Password must be greater than 4 characters")
         self.password = password
     def get_salary(self):
         return self.salary
     def get_password(self):
         return self.password
+    def __init__(self, salary, password):
+        self.set_salary(salary)
+        self.set_password(password)
+
