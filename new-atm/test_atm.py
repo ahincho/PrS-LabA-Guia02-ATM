c0e4ceae9e19e36c0d772bbd4eae5dfd848d23d4
dneirac <dneirac@unsa.edu.pe>
2023-05-18 15:42:48 -0500

    Caso de prueba de valores negativos

--- new-atm/test_atm.py
+++ new-atm/test_atm.py
@@ -10,11 +10,14 @@ import unittest
 # - Agregar alguna medida de segurida a la contrasenia
 # - Agregar una clase Enum con las opciones del menu
 # - A pesar de que falle 3 veces la contrasenia se muestra el menu para hacer operaciones
+# - No permitir cantidades negativas
 # Pruebas Unitarias:
 # - Crear un nuevo Cajero con un Usuario que tenga S./5000 y verificar saldo
 # - Al menos una prueba donde no te permita retirar mas de tu saldo
 # - Al menos una prueba donde no te permita retirar mas de S./3000
 # - Al menos una prueba donde no te permita depositar mas de S./3000
+# - Al menos una prueba donde no te permita depositar cantidades negativas 
+# - Al menos una prueba donde no te permita retirar cantidades negativas 
 
 class TestNewAtm(unittest.TestCase):
     def test_should_be_true(self):
@@ -38,6 +41,12 @@ class TestNewAtm(unittest.TestCase):
         atm = Cajero(user)
         with self.assertRaises(ValueError):
             atm.deposit(3200)
+    def test_amount_must_be_positive(self):
+        user = User(5000, "1234")
+        atm = Cajero(user)
+        with self.assertRaises(ValueError):
+            atm.withdraw(-3200)
+            atm.deposit(-3200)
 
 # Ejecutar pruebas unitarias
 if __name__ == "__main__":
