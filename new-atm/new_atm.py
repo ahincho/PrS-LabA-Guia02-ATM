a482a4105ba3c83854d1a3970bd1bb984e069978
ahincho <ahincho@unsa.edu.pe>
2023-05-17 20:57:23 -0500

    Agregando configuraciones. Valores maximos de deposito y retiro

--- new-atm/new_atm.py
+++ new-atm/new_atm.py
@@ -5,11 +5,15 @@
 @author: dneira 
 """
 
-import os
 from atm_options import ATM_Options as op
+from atm_config import ATM_Config as cfg
 from user import User
 
 class Cajero:
+    
+    current_deposit_amount = 0
+    current_withdraw_amount = 0
+    
     def __init__(self, user):
         self.set_user(user)
 
@@ -20,17 +24,19 @@ class Cajero:
         return self.user
     
     def withdraw(self, amount_withdraw):
-        if amount_withdraw > 3000:
-            raise ValueError("Amount must be less than 3000")
-        if self.user.get_salary() <  amount_withdraw: 
-            raise ValueError("Amount must be less than salary")
-        self.user.set_salary(self.user.get_salary()-amount_withdraw)
+        if (self.current_withdraw_amount + amount_withdraw) > cfg.MAX_WITHDRAW:
+            raise ValueError(f"You cant withdraw more than {cfg.MAX_WITHDRAW} in the same day")
+        if self.user.get_salary() < amount_withdraw: 
+            raise ValueError("Amount must be less than salary.")
+        self.user.set_salary(self.user.get_salary() - amount_withdraw)
+        self.current_withdraw_amount += amount_withdraw
         return self.user.get_salary()
 
     def deposit(self, amount_deposit):
-        if amount_deposit > 3000:
-            raise ValueError("Amount must be less than 3000")
-        self.user.set_salary(self.user.get_salary()+amount_deposit)
+        if (self.current_deposit_amount + amount_deposit) > cfg.MAX_DEPOSIT:
+            raise ValueError(f"You cant desposit more than {cfg.MAX_DEPOSIT} in the same day")
+        self.user.set_salary(self.user.get_salary() + amount_deposit)
+        self.current_deposit_amount += amount_deposit
         return self.user.get_salary()
 
     def view_salary(self):
@@ -50,14 +56,14 @@ class Cajero:
                     try:
                         amount_deposit = float(input("Amount to pay: "))
                         self.deposit(amount_deposit)
-                    except:
-                        print("Amount must be less than 3000. Try again.")
+                    except ValueError as ve:
+                        print(ve)
                 case op.WITHDRAW:
                     try:
                         amount_withdraw = float(input("Amount to withdraw: "))
                         self.withdraw(amount_withdraw)
-                    except:
-                        print("Amount must be less than 3000. Try again.")
+                    except ValueError as ve:
+                        print(ve)
                 case op.SHOW_SALARY:
                     print(f"Salary: {self.user.get_salary()}")
                 case op.EXIT:
@@ -65,9 +71,24 @@ class Cajero:
                     break
                 case _:
                     print("Invalid Option. Try again.")
+    
     def main(self):
-        self.show_menu()
+        auxChances = 0
+        while auxChances < cfg.MAX_CHANCES:
+            pwd = input("Give your password: ")
+            if pwd == self.user.get_password():
+                self.show_menu()
+                auxChances = 0
+            else:
+                auxChances += 1
+                print(f"Incorrect password. Try again. You got {cfg.MAX_CHANCES - auxChances} chances more.")
+        print("You lost all the chances.")
 
-# Run
-atm = Cajero(User(5000, "1234"))
-atm.main()
+def main():
+    # Run
+    atm = Cajero(User(5000, "1234"))
+    atm.main()
+
+if __name__ == "__main__":
+    main()
+    
