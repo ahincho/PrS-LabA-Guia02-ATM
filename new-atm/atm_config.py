b04a1a0167a621da96d5b9b4b4bfe95ca2856d34
ahincho <ahincho@unsa.edu.pe>
2023-05-17 20:56:01 -0500

    Clase Enum con las configuraciones del ATM

---
+++ new-atm/atm_config.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+"""
+@author: ahincho
+@author: dneira 
+"""
+
+from enum import Enum
+
+class ATM_Config():
+    MAX_CHANCES = 3
+    MAX_DEPOSIT = 3000
+    MAX_WITHDRAW = 3000
